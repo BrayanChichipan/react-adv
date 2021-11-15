e9a5f0f3e18db3f27128f985fcdddfca0bc1cbdd
chichipan <a1567149d@fie.umich.mx>
2021-11-15 13:01:25 -0600

    Component compund ready

---
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -0,0 +1,31 @@
+
+export interface ProductCardProps {
+    product: Product;
+    children?: React.ReactNode;
+}
+
+
+export interface Product {
+    id: string;
+    title: string;
+    img?: string;
+}
+
+export interface ProductContextProps {
+    counter: number;
+    increaseBy: (v: number) => void;
+    product: Product;
+}
+
+
+export interface ProductCardHOCProps{
+    ({ children, product }: ProductCardProps) : JSX.Element,
+    Title: ({ title }: {
+        title?: string | undefined;
+    }) => JSX.Element;
+    Image: ({ img }: {
+        img?: string | undefined;
+    }) => JSX.Element;
+    Buttons: () => JSX.Element;
+
+}
